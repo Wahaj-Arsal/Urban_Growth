bf0580ce318a6e9bc44fc15938224767c8da0160
102road <bill.brown094@gmail.com>
2022-03-25 10:01:46 +0000

    add react-app

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <nav className="navbar-container">
+        <button>
+          <img />
+          <h4>Discover</h4>
+        </button>
+        <button>
+          <img />
+          <h4>My Garden</h4>
+        </button>
+        <button>
+          <img />
+          <h4>Dashboard</h4>
+        </button>
+      </nav>
+    </div>
+  );
+}
